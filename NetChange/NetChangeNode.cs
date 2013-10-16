ceb3d5bde567a3fafa1ea8db73a28737088fc794
S.F.A.M. Brouwer <Blueation@gmail.com>
2013-10-16 03:31:51 +0200

    nodes now carry better info

--- NetChange/NetChangeNode.cs
+++ NetChange/NetChangeNode.cs
@@ -16,7 +16,7 @@ namespace NetChange {
         public short PortNumber { get { return this.value; } }
         public bool Updating { get; set; }
 
-        public Dictionary<short, int> distances = new Dictionary<short, int>();     //list of known/connected nodes and distances to the others from there
+        public Dictionary<short, Dictionary<short, int>> distances = new Dictionary<short, Dictionary<short, int>>();     //list of known/connected nodes and distances to the others from there
         public Dictionary<short, short> prefNeigh = new Dictionary<short, short>(); //to whom to send messages when it has to go to some node
 
         /// <summary>
@@ -24,6 +24,10 @@ namespace NetChange {
         /// </summary>
         /// <param name="portNumber">Port number to be used for this node</param>
         public NetChangeNode(short portNumber, bool startUpdating = false) : base(portNumber) {
+            Dictionary<short, int> temp = new Dictionary<short,int>();
+            temp.Add(PortNumber, 0);
+            distances.Add(PortNumber, temp);
+            prefNeigh.Add(PortNumber, PortNumber);
             Updating = startUpdating;
         }
             
@@ -34,16 +38,19 @@ namespace NetChange {
         public void AddNeighbor(NetChangeNode node) {
             base.AddNeighbor(node);
 
+            Dictionary<short, int> temp = new Dictionary<short, int>();
+            temp.Add(PortNumber, 1);
             if (distances.ContainsKey(node.value))
             {
-                distances[node.value] = 1;
+                distances[node.value] = temp;
             }
             else
             {
-                distances.Add(node.value, 1);
+                distances.Add(node.value, temp);
             }
 
-            Update();
+            foreach (NetChangeNode n in neighbors)
+                Update(n.value);
         }
 
         /// <summary>
@@ -52,6 +59,17 @@ namespace NetChange {
         /// <param name="portNumber">Port number of the new neighboring NetChange node</param>
         public override void AddNeighbor(short portNumber) {
             AddNeighbor(new NetChangeNode(portNumber));
+
+            Dictionary<short, int> temp = new Dictionary<short, int>();
+            temp.Add(PortNumber, 1);
+            if (distances.ContainsKey(portNumber))
+            {
+                distances[portNumber] = temp;
+            }
+            else
+            {
+                distances.Add(portNumber, temp);
+            }
         }
 
         /// <summary>
@@ -60,7 +78,7 @@ namespace NetChange {
         /// <param name="node">NetChange node to remove as neighbor</param>
         public void RemoveNeighbor(NetChangeNode node) {
             base.RemoveNeighbor(node);
-            Update();
+            Update(node.value);
         }
 
         /// <summary>
@@ -69,7 +87,7 @@ namespace NetChange {
         /// <param name="portNumber">Port number of neighbor to remove</param>
         public override void RemoveNeighbor(short portNumber) {
             base.RemoveNeighbor(portNumber);
-            Update();
+            Update(portNumber);
         }
 
         /// <summary>
@@ -91,8 +109,31 @@ namespace NetChange {
         /// <summary>
         /// Update this NetChange node
         /// </summary>
-        public void Update() {
+        public void Update(short portNumber) {
+            if (portNumber == PortNumber)
+            {
+                var temp = distances[portNumber];
+                temp[portNumber] = 0;
+                prefNeigh[portNumber] = portNumber;
+            }
+            else
+            {
+                KeyValuePair<short, int> d = minDist(distances[portNumber]);
+                if (d.Value + 1 < int.MaxValue)
+                {
+                    distances[PortNumber][portNumber] = d.Value + 1;
+                    prefNeigh[portNumber] = d.Key;
+                }
+            }
             // Update this node and neighbors
+        }
+
+        public KeyValuePair<short, int> minDist(Dictionary<short, int> dict)
+        {
+            KeyValuePair<short, int> result = new KeyValuePair<short, int>(short.MaxValue,int.MaxValue);
+            foreach (KeyValuePair<short, int> node in dict)
+                if (node.Value < result.Value) result = node;
+            return result;
         }
     }
     
